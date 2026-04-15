56e45354fe62370c41c7182d9b11a20c3be43035
kartik <kartikjethva0@gmail.com>
2026-04-15 13:36:53 +0530

    commit

--- Assessements/Assessement - 3 DB and Python Framework/BlogX/usserapp/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class UsserappConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'usserapp'
--- Assessements/Assessement - 3 DB and Python Framework/BlogX/usserapp/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path,include
-from usserapp import views
-
-urlpatterns = [
-    path('',views.index),
-    path('signup/',views.signup,name='signup'),
-    path('login/',views.login,name='login'),
-]
